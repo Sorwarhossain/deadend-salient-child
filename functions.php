45651d66ac681c4c9d25691c19e6289334c8c7be
Sorwarhossain <limonhossain57@gmail.com>
2017-11-22 23:24:19 +0600

    initialize files

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles');
+function salient_child_enqueue_styles() {
+	
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('font-awesome'));
+
+    if ( is_rtl() ) 
+   		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
+
+
+   	wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), false, true);
+}
+
+?>
