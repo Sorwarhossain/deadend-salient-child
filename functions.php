002e4d1d906aef6a60eeac3329e866dc22eb2f5b
Sorwarhossain <limonhossain57@gmail.com>
2017-11-28 23:31:48 +0600

    make some changes

--- functions.php
+++ functions.php
@@ -13,27 +13,85 @@ function salient_child_enqueue_styles() {
 }
 
 
-// add_filter( 'add_to_cart_text', 'woo_custom_cart_button_text',  1, 20);    // < 2.1
-// function woo_custom_cart_button_text() {
-//         return __( 'Find Store', 'woocommerce' );;
-// }
+
 
 
 add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );    // 2.1 +
  
-function woo_custom_cart_button_text() {
- 
-        return __( 'Find Store Location', 'woocommerce' );
- 
+function woo_custom_cart_button_text($text) {
+	global $product;
+ 	$id = $product->get_id();
+	$product_cats = wp_get_post_terms( $id, 'product_cat' );
+
+	$list_cat = array();
+	foreach($product_cats as $cat){
+		$list_cat[] = $cat->slug;
+	}
+
+ 	if(!in_array('apparel-decals', $list_cat)){
+ 		return __( 'Find Store Location', 'woocommerce' );
+ 	}
+ 	
+ 	return $text;
 }
+
+
 
 
 
 /**
- * Set a custom add to cart URL to redirect to
- * @return string
+ * Redirect users after add to cart.
  */
-function custom_add_to_cart_redirect() { 
-    return get_permalink( '6412' ); 
+function my_custom_add_to_cart_redirect( $url ) {
+	if ( ! isset( $_REQUEST['add-to-cart'] ) || ! is_numeric( $_REQUEST['add-to-cart'] ) ) {
+		return $url;
+	}
+	$product_id = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $_REQUEST['add-to-cart'] ) );
+	// Only redirect products that have the 't-shirts' category
+	if ( has_term( 'apparel-decals', 'product_cat', $product_id ) ) {
+		return $url;	
+	} else {
+		return $url = get_permalink( '6412' );
+	}
+	
 }
-add_filter( 'woocommerce_add_to_cart_redirect', 'custom_add_to_cart_redirect' );
+add_filter( 'woocommerce_add_to_cart_redirect', 'my_custom_add_to_cart_redirect' );
+
+
+
+
+
+
+
+
+
+/**
+ * customize login screen
+ *
+ */
+function deadend_custom_login_page() {
+    echo '<style type="text/css">
+        h1 a { background-image:url("'. get_stylesheet_directory_uri().'/images/logo.png") !important; height: 120px !important; width: 100% !important; margin: 0 auto !important; background-size: contain !important; }
+		h1 a:focus { outline: 0 !important; box-shadow: none; }
+        body.login { background-image:url("'. get_stylesheet_directory_uri().'/images/Login-banner.jpg") !important; background-repeat: no-repeat !important; background-attachment: fixed !important; background-position: center !important; background-size: cover !important; position: relative; z-index: 999;}
+  		body.login:before { background-color: rgba(0, 0, 0, 0.9); position: absolute; width: 100%; height: 100%; left: 0; top: 0; content: ""; z-index: -1; }
+  		.login form {
+  			background: rgba(255,255,255, 0.2) !important;
+  		}
+		.login form .input, .login form input[type=checkbox], .login input[type=text] {
+			background: transparent !important;
+			color: #ddd;
+		}
+		.login label {
+			color: #DDD !important;
+		}
+		.login #login_error, .login .message {
+			color: #ddd;
+			margin-top: 20px;
+			background: rgba(255,255,255, 0.2) !important;
+		}
+    </style>';
+}
+add_action('login_head', 'deadend_custom_login_page');
+
+
